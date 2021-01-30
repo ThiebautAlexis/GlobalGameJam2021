d74f87f264b21ced1fd605d1f977650e22814b59
LucasJoestar <lucasjoestar@outlook.fr>
2021-01-30 14:19:10 +0100

    Script Creator & Digger

---
+++ Assets/CORE/Controller/DiggerAttributes.cs
@@ -0,0 +1,27 @@
+// ===== Global Game Jam 2021 - https://github.com/ThiebautAlexis/GlobalGameJam2021 ===== //
+//
+// Notes:
+//
+// ====================================================================================== //
+
+using EnhancedEditor;
+using UnityEngine;
+
+namespace GlobalGameJam2021
+{
+    [CreateAssetMenu(fileName = "DAT_DiggerAttributes", menuName = "Datas/DiggerAttributes", order = 50)]
+	public class DiggerAttributes : ScriptableObject
+    {
+        #region Content
+        [HorizontalLine(1, order = 0), Section("DIGGER ATTRIBUTES", order = 1), Space(order = 2)]
+
+        public AnimationCurve SpawnSpeed = new AnimationCurve();
+        public AnimationCurve DigSpeed = new AnimationCurve();
+        public AnimationCurve AboutTurnSpeed = new AnimationCurve();
+
+        [HorizontalLine(1)]
+
+        public LayerMask layerMask = new LayerMask();
+		#endregion
+    }
+}
---
+++ Assets/EnhancedEditor/Scripts/Editor/ScriptCreator/ScriptCreator.cs
@@ -0,0 +1,96 @@
+﻿// ===== Global Game Jam 2021 - https://github.com/ThiebautAlexis/GlobalGameJam2021 ===== //
+//
+// Notes:
+//
+// ====================================================================================== //
+
+using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Tools
+{
+    public static class ScriptCreator
+    {
+        #region Content
+        private static readonly string templatePath = "/EnhancedEditor/Scripts/Editor/ScriptCreator/Templates/";
+
+        private static readonly string monoBehaviour = "MonoBehaviour.txt";
+        private static readonly string scriptableObject = "ScriptableObject.txt";
+
+        [MenuItem("Assets/Create/C# GGJ Script/MonoBehaviour", false, 50)]
+        public static void CreateMonoBehaviour() => ScriptCreatorWindow.GetWindow(monoBehaviour);
+
+        [MenuItem("Assets/Create/C# GGJ Script/ScriptableObject", false, 50)]
+        public static void CreateScriptableObject() => ScriptCreatorWindow.GetWindow(scriptableObject);
+
+        // -----------------------
+
+        public static void CreateScript(string _template, string _name)
+        {
+            string _path = AssetDatabase.GetAssetPath(Selection.activeObject.GetInstanceID());
+
+            string _fullPath = _path.Replace("Assets", Application.dataPath);
+            if (File.Exists(_fullPath))
+                _fullPath = Path.GetDirectoryName(_fullPath);
+
+            _fullPath += "/" + _name + ".cs";
+            if (File.Exists(_fullPath))
+            {
+                EditorUtility.DisplayDialog("Error", "A script with this name already exist.", "Okay");
+                return;
+            }
+
+            string[] _lines = File.ReadAllLines(Application.dataPath + templatePath + _template);
+            string _file = string.Empty;
+            for (int _i = 0; _i < _lines.Length; _i++)
+            {
+                _lines[_i] = _lines[_i].Replace("#SCRIPTNAME#", _name);
+                _file += _lines[_i] + "\n";
+            }
+
+            File.WriteAllText(_fullPath, _file);
+            AssetDatabase.Refresh();
+        }
+        #endregion
+    }
+
+    public class ScriptCreatorWindow : EditorWindow
+    {
+        #region Content
+        private string template = string.Empty;
+
+        public static void GetWindow(string _template)
+        {
+            ScriptCreatorWindow _launcher = (ScriptCreatorWindow)GetWindow(typeof(ScriptCreatorWindow));
+            _launcher.template = _template;
+
+            _launcher.maxSize = new Vector2(250, 50);
+            _launcher.minSize = new Vector2(250, 50);
+            _launcher.ShowUtility();
+        }
+
+        // -----------------------
+
+        private readonly GUIContent nameGUI = new GUIContent("Name:", "Name of the script.");
+        private readonly GUIContent createGUI = new GUIContent("Create", "Create a script with this name.");
+
+        private string scriptName = "NewScript";
+
+        private void OnGUI()
+        {
+            scriptName = EditorGUILayout.TextField(nameGUI, scriptName);
+
+            EditorGUILayout.Space(5);
+            EditorGUILayout.BeginHorizontal();
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button(createGUI))
+            {
+                ScriptCreator.CreateScript(template, scriptName);
+                Close();
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+        #endregion
+    }
+}
